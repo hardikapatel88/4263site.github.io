73da1716c2c7f29b48b0238314eaf4a406d9ce9b
HARnir <hpate11@lsu.edu>
2018-04-18 12:49:06 -0500

    changes

--- Background.js
+++ Background.js
@@ -32,7 +32,7 @@ function setup() {
   //canvas.position(100,100);
   canvas.parent('background-holder');
   background(backgroundColor);
-  //canvas.style('z-index', '-1');
+  canvas.style('z-index', '-1');
   var allWidth = (width - (NUMBER_COLUMN * SIZE_CIRCLE)) / 2;
   var allHeight = (height - ((NUMBER_LINE * SIZE_CIRCLE) + (NUMBER_LINE * SPACE_BETWEEN)-SPACE_BETWEEN)) /2;
 
